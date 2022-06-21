93a4218bf558f408f66dea2197e33bf1ebacc986
femiorok <68767356+femiorok@users.noreply.github.com>
2022-06-21 00:51:37 -0700

    Updates

---
+++ precache-manifest.16abe6d8bb9fc0d0b01a177022123e98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dcdd4364f0e6daec52d",
+    "url": "/sony-lens-library/static/css/main.bccf3425.chunk.css"
+  },
+  {
+    "revision": "4dcdd4364f0e6daec52d",
+    "url": "/sony-lens-library/static/js/main.4dcdd436.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/sony-lens-library/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/sony-lens-library/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "82c4b5e54c55947416f3",
+    "url": "/sony-lens-library/static/js/runtime~main.82c4b5e5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sony-lens-library/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fc8f89ba7a98d7afa0eba53f4e43b2d2",
+    "url": "/sony-lens-library/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sony-lens-library/precache-manifest.16abe6d8bb9fc0d0b01a177022123e98.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sony-lens-library/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
